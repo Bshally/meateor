6bc92ce278a2cc0ffe1167d19ac1c4a25e8a3009
Bshally <folder@seorigami.com>
2016-05-27 16:53:39 -0400

    just finished step 5 of tutorial.

--- simple-todos/imports/ui/App.jsx
+++ simple-todos/imports/ui/App.jsx
@@ -1,15 +1,27 @@
 import React, { Component, PropTypes } from 'react';
+import ReactDOM from 'react-dom';
 import { createContainer } from 'meteor/react-meteor-data';
 
-import Task from '../api/tasks.js';
-import Task from './Task.jsx';
+import { Tasks } from '../api/tasks.js';
 
+import Task from './Task.jsx';
 
 // App component - represents the whole app
 class App extends Component {
+    handleSubmit(event) {
+        event.preventDefault();
+        // Find the text field via the React ref
+        const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
+        Tasks.insert({
+           text,
+            createdAt: new Date(), // Current Time
+        });
+        //Clear the Form
+        ReactDOM.findDOMNode(this.refs.textInput).value = '';
+    }
 
     renderTasks() {
-        return this.props.tasks().map((task) => (
+        return this.props.tasks.map((task) => (
             <Task key={task._id} task={task} />
         ));
     }
@@ -19,8 +31,14 @@ class App extends Component {
             <div className="container">
                 <header>
                     <h1>Todo List</h1>
+                    <form className="new-task" onSubmit={this.handleSubmit.bind(this)}>
+                        <input
+                            type="text"
+                            ref="textInput"
+                            placeholder="Type to add new tasks"
+                        />
+                    </form>
                 </header>
-
                 <ul>
                     {this.renderTasks()}
                 </ul>
@@ -28,3 +46,13 @@ class App extends Component {
         );
     }
 }
+
+App.propTypes = {
+    tasks: PropTypes.array.isRequired,
+};
+
+export default createContainer(() => {
+    return {
+        tasks: Tasks.find({}, { sort: { createdAt: -1 }}).fetch(),
+    };
+}, App);
